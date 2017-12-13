4d911d6e03a1333c80115b9e01d1428888d4cb43
skundu <spondon6102@gmail.com>
2017-12-13 12:01:00 -0500

    Added extra credit portion

--- gamedesign.js
+++ gamedesign.js
@@ -5,18 +5,29 @@
 //Description of this game: http://cgclass.csc.ncsu.edu/search/label/homework
 
 //================================================================================
+
+
 var scene,renderer,camera;
 var aSTEP,dSTEP;
-var totalMissilesRound1;
-
-const WIDTH = 600;//window.innerWidth;
-const HEIGHT = 600;//window.innerHeight;
-
+var totalMissilesRound;
+var building = [];
+var missileBase = [];
 var aMissile =[];// = new THREE.Vector3();
 var aMissileEnd = [];
 var x1,y1;
+var bringUFO = true;
+var score = 0;
+var currentLevel = 1;
+var delayinms= 1000;
+var delayinA= 3000;
+var sphereUFO,sphereEx1,sphereEx2;
+var flag = 0;
+var explosionSound = new Audio('./assets/Bomb_Exploding.mp3');
+var batteryExplosionSound = new Audio('./assets/Shotgun_Blast.mp3');
 
 
+const WIDTH = 600;//window.innerWidth;
+const HEIGHT = 600;//window.innerHeight;
 
 // Set the scene size.
 function init() {
@@ -31,7 +42,7 @@ function init() {
 
     dSTEP = 0.3; // essentially the refresh rate - for the ballistic missile
     aSTEP = 0.4; // refresh rate for the anti-ballistic missile
-    totalMissilesRound1 = 40;
+    totalMissilesRound = 40;
 
     // Get the DOM element to attach to
     const container =
@@ -58,35 +69,35 @@ function init() {
     // Start the renderer.
     renderer.setSize(WIDTH, HEIGHT);
 
-    // var axes = new THREE.AxesHelper(100);
-    // scene.add(axes);
-
     // Attach the renderer-supplied
     // DOM element.
     container.appendChild(renderer.domElement);
 
     // text for Missile Command
 
-    /*var loader = new THREE.FontLoader();
+    var loader = new THREE.FontLoader();
 
 	loader.load( 'https://raw.githubusercontent.com/sequae92/MissileCommand3D/master/fonts/helvetiker_bold.typeface.json', function ( font ) {
 
 	var geometry = new THREE.TextGeometry( 'MISSILE COMMAND 3D', {
 		font: font,
-		size: 13,
+		size: 10,
 		height: 3,
 	} );
-	var txtmaterial = new THREE.MeshBasicMaterial({ color: 0xff0000});
+	var txtmaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff});
     var txtObject = new THREE.Mesh(geometry, txtmaterial);
 
     txtObject.position.z = -300;
-    txtObject.position.x = -98;
-    txtObject.position.y = 95;
+    txtObject.position.x = -80;
+    txtObject.position.y = 110;
 
     scene.add(txtObject); 
 
     } );
-    */
+
+    // this is for scoreboard
+
+    
     // Load the background texture
     var bckgTexture = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/sequae92/MissileCommand3D/master/assets/skystarstexture2.jpg' );
     var backgroundMesh = new THREE.Mesh(
@@ -151,67 +162,42 @@ function init() {
 	buildingTexture.repeat.set( 4, 4 ); 
 	buildingTexture.needsUpdate = true;
 
-/*	//defining the geometry of the building
-
-	var buildingPts = [];
-	buildingPts.push( new THREE.Vector2 ( 200, 300 ) );
-	buildingPts.push( new THREE.Vector2 ( 400, 500 ) );
-	buildingPts.push( new THREE.Vector2 ( 200, 300 ) );
-	buildingPts.push( new THREE.Vector2 ( 400, 500 ) );
-
-	var buildingShape = new THREE.Shape(buildingPts);
-	var extrudeSettings = { amount: 40 }; // bevelSegments: 2, steps: 2 , bevelSegments: 5, bevelSize: 8, bevelThickness:5
-
-	var buildingGeometry = new THREE.ExtrudeGeometry(buildingShape,extrudeSettings);
-	var buildingMaterial = new THREE.MeshBasicMaterial( {side: THREE.DoubleSide, map: buildingTexture} );
-	var object = new THREE.Mesh( buildingGeometry, buildingMaterial );
-	
-	object.position.z = -1000;
-	object.position.y = -70;
-	//plane.position.x = -100;
-
-	object.rotation.x = -Math.PI/2.5;
-	scene.add(object);
-*/
-
-
-
 	var buildingGeometry = new THREE.BoxGeometry( 15, 40, 50 );
 	var buildingMaterial = new THREE.MeshBasicMaterial( {side: THREE.DoubleSide, map: buildingTexture} );
-	var building1 = new THREE.Mesh( buildingGeometry, buildingMaterial );
+	building[0] = new THREE.Mesh( buildingGeometry, buildingMaterial );
 
-	building1.position.z = -300;
-	building1.position.y = -35;
-	building1.position.x = -50;
-	scene.add(building1);
+	building[0].position.z = -320;
+	building[0].position.y = -35;
+	building[0].position.x = -50;
+	scene.add(building[0]);
 
-	var building2 = new THREE.Mesh( buildingGeometry, buildingMaterial );
+	building[1] = new THREE.Mesh( buildingGeometry, buildingMaterial );
 
-	building2.position.z = -300;
-	building2.position.y = -35;
-	building2.position.x = -25;
-	scene.add(building2);
+	building[1].position.z = -320;
+	building[1].position.y = -35;
+	building[1].position.x = -25;
+	scene.add(building[1]);
 
-	var building3 = new THREE.Mesh( buildingGeometry, buildingMaterial );
+	building[2] = new THREE.Mesh( buildingGeometry, buildingMaterial );
 
-	building3.position.z = -300;
-	building3.position.y = -35;
-	building3.position.x = 0;
-	scene.add(building3);
+	building[2].position.z = -320;
+	building[2].position.y = -35;
+	building[2].position.x = 0;
+	scene.add(building[2]);
 
-	var building4 = new THREE.Mesh( buildingGeometry, buildingMaterial );
+	building[3] = new THREE.Mesh( buildingGeometry, buildingMaterial );
 
-	building4.position.z = -300;
-	building4.position.y = -35;
-	building4.position.x =  25;
-	scene.add(building4);
+	building[3].position.z = -320;
+	building[3].position.y = -35;
+	building[3].position.x =  25;
+	scene.add(building[3]);
 
-    var building5 = new THREE.Mesh( buildingGeometry, buildingMaterial );
+    building[4] = new THREE.Mesh( buildingGeometry, buildingMaterial );
 
-    building5.position.z = -300;
-    building5.position.y = -35;
-    building5.position.x =  50;
-    scene.add(building5);
+    building[4].position.z = -320;
+    building[4].position.y = -35;
+    building[4].position.x =  50;
+    scene.add(building[4]);
 
     //defining the missile base texture
 
@@ -224,22 +210,22 @@ function init() {
     missileBaseTexture.repeat.set( 4, 4 ); 
     missileBaseTexture.needsUpdate = true;
 
-    var missileBaseGeometry = new THREE.BoxGeometry( 20, 50, 50 );
+    var missileBaseGeometry = new THREE.BoxGeometry( 25, 40, 50 );
     var missileBaseMaterial = new THREE.MeshBasicMaterial( {side: THREE.DoubleSide, map: missileBaseTexture} );
     
-    var missileBase1 = new THREE.Mesh( missileBaseGeometry, missileBaseMaterial );
+    building[5] = new THREE.Mesh( missileBaseGeometry, missileBaseMaterial ); // This is missileBase 1
 
-    missileBase1.position.z = -300;
-    missileBase1.position.y = -35;
-    missileBase1.position.x = -85;
-    scene.add(missileBase1);
+    building[5].position.z = -320;
+    building[5].position.y = -35;
+    building[5].position.x = -85;
+    scene.add(building[5]);
 
-    var missileBase2 = new THREE.Mesh( missileBaseGeometry, missileBaseMaterial );
+    building[6] = new THREE.Mesh( missileBaseGeometry, missileBaseMaterial ); // This is missileBase 2
 
-    missileBase2.position.z = -300;
-    missileBase2.position.y = -35;
-    missileBase2.position.x = 85;
-    scene.add(missileBase2);
+    building[6].position.z = -320;
+    building[6].position.y = -35;
+    building[6].position.x = 85;
+    scene.add(building[6]);
 
     //defining the descending missile object
     // define an array of missiles
@@ -285,7 +271,7 @@ function init() {
    
         //for dropmissiles
     myTime = setInterval(function(){
-     if(missileCount <= totalMissilesRound1)
+     if(missileCount <= totalMissilesRound)
         {
             dropMissiles();
             //console.log('A');
@@ -325,8 +311,6 @@ function init() {
 
             init();
             animate();
-        // // var canvas = document.getElementById("container");
-        // document.addEventListener("mousedown",onMouseClick, false);
     }
 
     function onMouseClick(event)
@@ -380,30 +364,15 @@ function init() {
 
     scene.add(amissile);
     aMissile.push(amissile);
-    // console.log('A');
-    //directABMissile();
-
   }
 
     function directABMissile(){
 
-        // for(var i=0;i)
-        // console.log(aMissile.length);
         for(var i=0;i<aMissile.length;i++){
-            // initial position of the AB missile
-            // iZ = -300;      
-            // iX = -35;
-            // iY = -85;
-
-            // direction vector for movement
-            // var direction = new THREE.Vector3( x1-iX, y1-iY, 0);
+            
 
             // scalar to simulate speed
             var speed = 0.05;
-            // var vector = direction.multiplyScalar( speed, speed, 0 );
-
-            // console.log(aMissile[0].x,aMissile[0].y);
-            // if()
 
             aMissile[i].position.x += (aMissileEnd[i].x-aMissile[i].position.x)*speed;//vector.x;
             aMissile[i].position.y += (aMissileEnd[i].y-aMissile[i].position.y)*speed;//vector.y;
@@ -413,10 +382,44 @@ function init() {
                 aMissileEnd.splice(i,1);
             }
         }
-        // console.log(aMissile.x,aMissile.y);
-        //object.z += vector.z;
-
     }
+
+    function updateScore(increment){
+
+        score+=increment; // each missile-AB missile collision earns you 1 point
+        document.getElementById("scores").innerHTML = "Score:" + score;
+    }
+
+    function dispUFO(){
+
+        var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+        var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+        sphereUFO = new THREE.Mesh( geometry, material );
+        
+        sphereUFO.position.z = -250;
+        sphereUFO.position.y = 50;
+        sphereUFO.position.x = 100;
+// 
+        scene.add( sphereUFO );
+        //console.log('A');
+        flag = 1;
+    }
+
+    function moveUFO(){
+
+        sphereUFO.position.x -= dSTEP;
+            if((sphereUFO.position.x <= -300)){
+                scene.remove(sphereUFO);
+                //cflag =0;
+                }
+    }
+
+    //for UFO
+    myTime = setInterval(function(){
+     if(flag==0)
+        {
+            dispUFO();
+        }},5000);
 
     function checkColisions(){
 
@@ -425,50 +428,159 @@ function init() {
                 for(var j=0; j < dMissile.length;j++){
                         if((dMissile[j].position.x >= (aMissile[i].position.x-5) && dMissile[j].position.x <= (aMissile[i].position.x+5)) && 
                         (dMissile[j].position.y >= (aMissile[i].position.y-5) && dMissile[j].position.y <= (aMissile[i].position.y+5) )){
+                                
+                                createSmallExplosion(dMissile[j].position.x,dMissile[j].position.y);
                                 scene.remove(dMissile[j]);
                                 scene.remove(aMissile[i]);
                                 aMissile.splice(i,1);
                                 dMissile.splice(j,1);
                                 aMissileEnd.splice(i,1);
+                                explosionSound.play();
+                                updateScore(1);
+                                break;
+                                //console.log(score);
                         }
                 }
             }
 
         //check the intersection of missile and buildings ( and batteries)
 
-        //for(var i=0; i < aMissile.length;i++){
+        for(var i=0; i < dMissile.length;i++){
+            for(var j=0;j < building.length;j++){
 
-        //}
+                if((dMissile[i].position.x >= (building[j].position.x-5) && dMissile[i].position.x <= (building[j].position.x+5)) && 
+                        (dMissile[i].position.y >= (building[j].position.y-5) && dMissile[i].position.y <= (building[j].position.y+5) )){
+                                
+                                createBigExplosion(building[j].position.x,building[j].position.y);
+                                scene.remove(dMissile[i]);
+                                scene.remove(building[j]);
+                                dMissile.splice(i,1);
+                                building.splice(j,1);
+                                batteryExplosionSound.play();
+                        }
+            }
+
+        }
+
+        //check the collisions wof missile and UFO
+
+        for(var i=0; i < aMissile.length;i++){
+
+            if((aMissile[i].position.x >= (sphereUFO.position.x-5) && aMissile[i].position.x <= (sphereUFO.position.x+5)) && 
+                        (aMissile[i].position.y >= (sphereUFO.position.y-5) && aMissile[i].position.y <= (sphereUFO.position.y+5) )){
+
+                                //createExplosion(sphereUFO.position.x,sphereUFO.position.y)
+                                scene.remove(aMissile[i]);
+                                scene.remove(sphereUFO);
+                                aMissile.splice(i,1);
+                                explosionSound.play();
+                                updateScore(5);
+                        }
+        }
+
+    }
+
+    // this is to animate missile-missile and missile-city explosions
+    function createBigExplosion(a,b){
+
+        var geometry = new THREE.SphereGeometry( 15, 32, 32 );
+
+        var material = new THREE.MeshBasicMaterial( {color: 0xffa500} );
+        sphereEx1 = new THREE.Mesh( geometry, material );
+            
+        sphereEx1.position.x = a;
+        sphereEx1.position.y = b;
+        sphereEx1.position.z = -300;
+
+        scene.add( sphereEx1 );
+
+        setTimeout(function() {
+        //your code to be executed after 1 second
+        scene.remove(sphereEx1);
+        }, delayinms);
+    }
+
+     function createSmallExplosion(a,b){
+
+        var geometry = new THREE.SphereGeometry( 5, 32, 32 ); 
+
+        var material = new THREE.MeshBasicMaterial( {color: 0xffa500} );
+        sphereEx2 = new THREE.Mesh( geometry, material );
+            
+        sphereEx2.position.x = a;
+        sphereEx2.position.y = b;
+        sphereEx2.position.z = -300;
+
+        scene.add( sphereEx2 );
+
+        setTimeout(function() {
+        //your code to be executed after 1 second
+        scene.remove(sphereEx2);
+        }, delayinms);
+    }
+
+
+
+    function nextLevel(){
+
+        dSTEP+=0.2;
+        missileCount = 0;
+        //score = 0;
+        currentLevel+=1;
+        displayNextLevel();
+        document.getElementById("level").innerHTML = "Level:" + currentLevel;
+    }
+
+    function displayNextLevel(){
+
+    var loader = new THREE.FontLoader();
+
+    loader.load( 'https://raw.githubusercontent.com/sequae92/MissileCommand3D/master/fonts/helvetiker_bold.typeface.json', function ( font ) {
+
+    var geometry = new THREE.TextGeometry( 'BE READY -> NEXT LEVEL', {
+        font: font,
+        size: 7,
+        height: 3,
+    } );
+    var txtmaterial = new THREE.MeshBasicMaterial({ color: 0x00ffff});
+    var levelObject = new THREE.Mesh(geometry, txtmaterial);
+
+    levelObject.position.z = -300;
+    levelObject.position.x = -70;
+    levelObject.position.y = 50;
+
+    scene.add(levelObject);
+
+    setTimeout(function() {
+  //your code to be executed after 1 second
+    scene.remove(levelObject);
+    }, delayinms);
+
+     } );
 
     }
     
-    function animate(){
-        
+    function animate(){   
          //renderer.autoClear = false;
          //renderer.clear();
          
-         requestAnimationFrame(animate);
-         renderer.render(scene,camera);
+        requestAnimationFrame(animate);
+        renderer.render(scene,camera);
 
-        //changing the direction of the missile iteratively 
-        //dMissile.position.add(direction1);
-        //dropMissiles();
+        if(missileCount == totalMissilesRound)
+        {
+            nextLevel();
+        }
 
         moveMissiles();
 
         directABMissile();
 
+        if(flag == 1){
+            moveUFO();
+        }
+
         checkColisions();
 
-        //setInterval(moveMissiles,500);
-        //setInterval(calculateXY,500);
 
-        //setInterval(directABMissile,500);
-
-        //aMissile.position.add(direction2);
-            
     }
-    
-    //myTime = setInterval(moveMissiles, 3000);
-
-     //requestAnimationFrame(animate);
